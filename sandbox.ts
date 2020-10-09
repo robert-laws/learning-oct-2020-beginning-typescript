8cfdf58e0aa371751e5cc8b6755ae7c6e9907e05
Robert Laws <roblaws@me.com>
2020-10-09 09:54:23 +0300

    basics of explicit and dynamic types

--- sandbox.ts
+++ sandbox.ts
@@ -13,3 +13,57 @@ item = {
   name: 'bill',
   size: 5,
 };
+
+// explicit types
+let firstName: string;
+let price: number;
+let isLoggedIn: boolean;
+
+price = 43;
+isLoggedIn: false;
+firstName = 'Hal';
+
+// arrays
+let items: string[];
+items.push('pencil')
+items = ['brush', 'stuff'];
+
+// union types
+let stuff: (string|number)[] = [];
+stuff.push('hello')
+stuff.push(35)
+
+let uid: string|number;
+uid = 236272;
+uid = '32425'
+
+// objects
+let playerOne: object;
+playerOne = { name: 'bob', age: 34 }
+
+let playerTwo: {
+  name: string,
+  age: number,
+  isManager: boolean
+}
+
+playerTwo = {
+  name: 'Kal',
+  age: 34,
+  isManager: true
+}
+
+let locations: (object)[];
+locations.push({ name: 'Chicago'})
+
+// dynamic - any - type
+let place: any = 25;
+place = 'home';
+
+let randomItems: any[] = [];
+randomItems.push(5);
+randomItems.push('hello')
+randomItems.push(true)
+
+let people: { name: any, age: any }
+people = { name: 5, age: '5' }
