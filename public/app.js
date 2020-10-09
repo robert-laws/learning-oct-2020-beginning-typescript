5a925b5f29f7043a98fb2f21c91e48ef94d7f105
Robert Laws <roblaws@me.com>
2020-10-09 16:39:56 +0300

    reset for 2nd part of tutorial

--- public/app.js
+++ public/app.js
@@ -1,1 +1,6 @@
 "use strict";
+// const anchor = document.querySelector('a')!; // add exclamation mark to override error
+// console.log(anchor.href);
+// const myForm = document.querySelector('form')!;
+var myForm = document.querySelector('.new-item-form');
+console.log(myForm.children);
---
+++ src/app.ts
@@ -0,0 +1,8 @@
+// const anchor = document.querySelector('a')!; // add exclamation mark to override error
+
+// console.log(anchor.href);
+
+// const myForm = document.querySelector('form')!;
+
+const myForm = document.querySelector('.new-item-form') as HTMLFormElement;
+console.log(myForm.children);
