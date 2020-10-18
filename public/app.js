a8e07e05f39f7403b4bba8a013aa8357bede4ad8
Robert Laws <roblaws@me.com>
2020-10-18 11:07:51 +0300

    basic examples

--- public/app.js
+++ public/app.js
@@ -1,6 +1,59 @@
 "use strict";
-// const anchor = document.querySelector('a')!; // add exclamation mark to override error
-// console.log(anchor.href);
-// const myForm = document.querySelector('form')!;
-var myForm = document.querySelector('.new-item-form');
-console.log(myForm.children);
+// explicit types
+var character;
+var age;
+var isLoggedIn;
+character = 'Hal';
+// arrays
+var items = [];
+var stuff = [];
+// union types
+var person;
+person = 'Bob';
+person = 4;
+var sale = {
+    id: 4,
+    name: 'Hat',
+};
+var myAge;
+myAge = 35;
+myAge = ['hello'];
+// functions
+var greet;
+greet = function () {
+    console.log('hello there!');
+};
+var add = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    console.log(c);
+    return a + b;
+};
+console.log(add(5, 7, 40));
+var logDetails = function (user) {
+    console.log(user.name);
+};
+logDetails({ uid: '5', name: 'Bob' });
+// function signature
+var welcome;
+welcome = function (name, greeting) {
+    console.log(name + " says " + greeting);
+};
+welcome('Kal', 'How are you?');
+var calc;
+calc = function (x, y, action) {
+    if (action === 'add') {
+        return x + y;
+    }
+    else if (action === 'subtract') {
+        return x - y;
+    }
+    else {
+        return -1;
+    }
+};
+console.log(calc(5, 8, 'add'));
+var logPerson;
+logPerson = function (myPerson) {
+    console.log("hello, " + myPerson.name + " ---- age: " + myPerson.age.toString());
+};
+logPerson({ name: 'Mel', age: 35 });
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,86 @@
-// const anchor = document.querySelector('a')!; // add exclamation mark to override error
+// explicit types
+let character: string;
+let age: number;
+let isLoggedIn: boolean;
 
-// console.log(anchor.href);
+character = 'Hal';
 
-// const myForm = document.querySelector('form')!;
+// arrays
+let items: string[] = [];
+let stuff: (string | number)[] = [];
 
-const myForm = document.querySelector('.new-item-form') as HTMLFormElement;
-console.log(myForm.children);
+// union types
+let person: string | number;
+person = 'Bob';
+person = 4;
+
+// objects
+type StoreSale = {
+  id: number;
+  name: string;
+};
+
+let sale: StoreSale = {
+  id: 4,
+  name: 'Hat',
+};
+
+let myAge: any;
+myAge = 35;
+myAge = ['hello'];
+
+// functions
+let greet: Function;
+
+greet = () => {
+  console.log('hello there!');
+};
+
+const add = (a: number, b: number, c: number = 10) => {
+  console.log(c);
+  return a + b;
+};
+
+console.log(add(5, 7, 40));
+
+type StrOrNum = string | number;
+
+type User = {
+  uid: StrOrNum;
+  name: string;
+};
+
+const logDetails = (user: User) => {
+  console.log(user.name);
+};
+
+logDetails({ uid: '5', name: 'Bob' });
+
+// function signature
+let welcome: (a: string, b: string) => void;
+welcome = (name: string, greeting: string) => {
+  console.log(`${name} says ${greeting}`);
+};
+welcome('Kal', 'How are you?');
+
+let calc: (a: number, b: number, c: string) => number;
+calc = (x: number, y: number, action: string) => {
+  if (action === 'add') {
+    return x + y;
+  } else if (action === 'subtract') {
+    return x - y;
+  } else {
+    return -1;
+  }
+};
+console.log(calc(5, 8, 'add'));
+
+type Person = {
+  name: string;
+  age: number;
+};
+let logPerson: (myObj: Person) => void;
+logPerson = (myPerson: { name: string; age: number }) => {
+  console.log(`hello, ${myPerson.name} ---- age: ${myPerson.age.toString()}`);
+};
+logPerson({ name: 'Mel', age: 35 });
